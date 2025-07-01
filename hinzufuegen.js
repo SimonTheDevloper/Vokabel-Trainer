2b2d66e4b9e61802fbb8e757b374e1d94ce1e98f
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-07-01 16:43:28 +0200

    hoffentlich geht das

--- hinzufuegen.js
+++ hinzufuegen.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const vocabList = []
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,35 +28,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
         console.log("Aktualisierte Vokabelliste:", vocabList);
     });
-=======
-const vocabList = []
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    const addVocabForm = document.getElementById('addVocabForm');
-    addVocabForm.addEventListener('submit', function(event) {
-
-        event.preventDefault();
-        const deutschesWort = document.getElementById('deutschesWort').value.trim();
-        const englischesWort = document.getElementById('englischesWort').value.trim();
-        const kategorie = document.getElementById('kategorie').value.trim();
-
-        // Neues Vokabelobjekt erstellen
-            const neueVokabel = {
-            word: deutschesWort,
-            translation: englischesWort,
-            category: kategorie,
-        };
-        // Hier wird ein neues JavaScript-Objekt namens 'neueVokabel' erstellt.
-        // Dieses Objekt hat Schlüssel ('word', 'translation', 'category', 'note', 'favorite')
-        vocabList.push(neueVokabel);
-
-        alert(`Vokabel "${deutschesWort} - ${englischesWort}" wurde hinzugefügt.`);
-        addVocabForm.reset();
-
-        localStorage.setItem('vokabelListe', JSON.stringify(vocabList));
-
-        console.log("Aktualisierte Vokabelliste:", vocabList);
-    });
->>>>>>> 7528ccca3164b2ca9550f7fbd122b7b07fe5927d
 });
