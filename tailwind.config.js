50c3d7516c724068973ab30964a099c2c83f1093
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-07-29 18:20:04 +0200

    Alte Version von vokabeltester.html aus Commit 78ef8d6 wiederhergestellt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./voktestmitids.html" // Sucht in deiner index.html im Stammverzeichnis
+    "./voktestmitids.html ./vokabeltester.html" // Sucht in deiner index.html im Stammverzeichnis
     // "./src/**/*.{html,js,ts}", // Beispiel: Falls du HTML, JS oder TS in einem 'src'-Ordner hast (optional)
     // Füge hier weitere Pfade hinzu, wenn du mehr HTML-Dateien oder JS/TS hast, die HTML generieren
   ],
