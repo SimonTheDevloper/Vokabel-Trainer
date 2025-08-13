226709d7f863ad9e96486446830050ca51899e9a
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-08-13 13:38:26 +0200

    Update- container auf der Startseite in einen slider gemacht.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
     "./html/wörterbuch.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'app-gradient': "linear-gradient(135deg, #2563eb, #0ea5e9, #14b8a6)"
+      },
+      animation: {
+        'gradient-flow': 'gradient-flow 12s ease infinite',
+      },
+      keyframes: {
+        'gradient-flow': {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' },
+        }
+      }
+    }
   },
   plugins: [],
 }
