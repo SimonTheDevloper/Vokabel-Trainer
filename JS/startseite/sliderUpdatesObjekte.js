ceb34ef04aa9bf26c5995499d5cce331146f8635
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-09-14 12:54:45 +0200

    UpdatesObjekte jetzt in einer anderen Datei

---
+++ JS/startseite/sliderUpdatesObjekte.js
@@ -0,0 +1,42 @@
+export const updates = [
+    {
+        "version": "3.2.2",
+        "versionColor": "slate",
+        "isNew": true,
+        "title": "Wörterbuch ist leer wieder hinzugefügt",
+        "description": "Wenn keine Vokabeln Hinzugefügt wurden, ploppt bei der Abfrage ein Kasten auf",
+        "tags": ["UX/UI", "Feature", "Abfrage"]
+    },
+    {
+        "version": "3.2.1",
+        "versionColor": "lime",
+        "isNew": false,
+        "title": "Slider bei der Startseite hinzugefügt",
+        "description": "Besserer Überblick über die verschiedensten Versionen.",
+        "tags": ["UX/UI"]
+    },
+    {
+        "version": "3.1",
+        "versionColor": "indigo",
+        "isNew": false,
+        "title": "Neues Design bei der Abfrage",
+        "description": "Die Abfrage wurde mit einer neuen Benutzeroberfläche und einer verbesserten Benutzererfahrung überarbeitet.",
+        "tags": ["UI/UX", "Design"]
+    },
+    {
+        "version": "3.0",
+        "versionColor": "indigo",
+        "isNew": false,
+        "title": "Unser größtes Update aller Zeiten",
+        "description": "Alle Oberflächen wurden von Grund auf neu gestaltet mit Fokus auf Benutzerfreundlichkeit und modernem Aussehen.",
+        "tags": ["UI-Redesign"]
+    },
+    {
+        "version": "2.0",
+        "versionColor": "amber",
+        "isNew": false,
+        "title": "Neue Funktionen und Verbesserungen",
+        "description": "Neues Startseiten-Design, Vokabel-Hinzufügen mit Kategorien, Tests nach Kategorien und zufällige Abfrage-Reihenfolge.",
+        "tags": ["Features", "Kategorien"]
+    }
+];
--- JS/startseite/sliderUpdatesgenerier.js
+++ JS/startseite/sliderUpdatesgenerier.js
@@ -1,45 +1,4 @@
-const updates = [
-    {
-        "version": "3.2.2",
-        "versionColor": "slate",
-        "isNew": true,
-        "title": "Wörterbuch ist leer wieder hinzugefügt",
-        "description": "Wenn keine Vokabeln Hinzugefügt wurden, ploppt bei der Abfrage ein Kasten auf",
-        "tags": ["UX/UI", "Feature", "Abfrage"]
-    },
-    {
-        "version": "3.2.1",
-        "versionColor": "lime",
-        "isNew": false,
-        "title": "Slider bei der Startseite hinzugefügt",
-        "description": "Besserer Überblick über die verschiedensten Versionen.",
-        "tags": ["UX/UI"]
-    },
-    {
-        "version": "3.1",
-        "versionColor": "indigo",
-        "isNew": false,
-        "title": "Neues Design bei der Abfrage",
-        "description": "Die Abfrage wurde mit einer neuen Benutzeroberfläche und einer verbesserten Benutzererfahrung überarbeitet.",
-        "tags": ["UI/UX", "Design"]
-    },
-    {
-        "version": "3.0",
-        "versionColor": "indigo",
-        "isNew": false,
-        "title": "Unser größtes Update aller Zeiten",
-        "description": "Alle Oberflächen wurden von Grund auf neu gestaltet mit Fokus auf Benutzerfreundlichkeit und modernem Aussehen.",
-        "tags": ["UI-Redesign"]
-    },
-    {
-        "version": "2.0",
-        "versionColor": "amber",
-        "isNew": false,
-        "title": "Neue Funktionen und Verbesserungen",
-        "description": "Neues Startseiten-Design, Vokabel-Hinzufügen mit Kategorien, Tests nach Kategorien und zufällige Abfrage-Reihenfolge.",
-        "tags": ["Features", "Kategorien"]
-    }
-];
+import { updates } from './sliderUpdatesObjekte.js'
 const sliderWrapper = document.querySelector('.slider-wrapper');
 sliderWrapper.innerHTML = ''; // Leert den Container 
 
@@ -62,7 +21,7 @@ updates.forEach(update => {
                 </div>
                 <div class="flex flex-wrap gap-2 mt-6">
                     ${tagsHtml}
-                </div>
+                </div> 
             </div>
         </div>
     `;
