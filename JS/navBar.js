3f85045c0a5cf428364220cb72e27822400bb608
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-09-16 17:46:30 +0200

    feat: bottem nav bar, die anzeigt auf welcher seite man sich befindet das ist dann makiert

--- JS/navBar.js
+++ JS/navBar.js
@@ -11,7 +11,7 @@ const mobileNavHTML = `
   </div>
 </header>
 
-<nav class="fixed bottom-0 left-0 right-0 z-50 bg-indigo-600 shadow-md md:hidden">
+<nav id="bottemNav" class="fixed bottom-0 left-0 right-0 z-50 bg-indigo-600 shadow-md md:hidden">
     <div class="mx-auto flex h-16 justify-around">
         <a href="../index.html" class="flex flex-1 flex-col items-center justify-center px-2 py-3 text-white hover:bg-indigo-700 transition-colors duration-200">
             <i class="material-icons text-xl">home</i>
@@ -34,3 +34,25 @@ const mobileNavHTML = `
 `;
 document.body.insertAdjacentHTML('afterbegin', mobileNavHTML); //fügt die Navbar in den Body teil ein
 
+const path = decodeURIComponent(window.location.pathname.split('/').pop()); // decodeURIComponent damit wird "ü" problem behoben :)
+const hrefs = [];
+/*let lee = "apple";
+const fruits = ['apple', 'banana', 'orange'];
+for (const fruit of fruits) {
+  if (lee == fruit) {
+    console.log("ja")
+  } else {
+    console.log("ne")
+  }
+} */
+console.log(path)
+const navLinks = document.querySelectorAll('#bottemNav a') // sucht nur nach der Bottem navBar
+console.log(navLinks.length);
+console.log(hrefs)
+
+for (const link of navLinks) {
+  if (path === link.getAttribute('href')) {
+    link.classList.remove('text-white', 'hover:bg-indigo-700');
+    link.classList.add('bg-indigo-700', 'text-amber-400');
+  }
+}
