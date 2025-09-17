0d72411ba3cb48e7eaf0abcb1884b2b151a0d482
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-09-17 17:04:20 +0200

    favicon hinzugefügt

--- JS/navBar.js
+++ JS/navBar.js
@@ -3,7 +3,7 @@ const mobileNavHTML = `
   <div class="container mx-auto flex items-center justify-between px-4 py-3">
     <span class="text-xl font-bold text-amber-400">Vokabel Trainer</span>
     <nav class="flex space-x-4">
-      <a href="index.html" class="hover:bg-indigo-700 px-3 py-2 rounded-md font-semibold">Startseite</a>
+      <a href="../index.html" class="hover:bg-indigo-700 px-3 py-2 rounded-md">Startseite</a>
       <a href="voktestmitids.html" class="hover:bg-indigo-700 px-3 py-2 rounded-md">Vokabel-Test</a>
       <a href="hinzufügen.html" class="hover:bg-indigo-700 px-3 py-2 rounded-md">Hinzufügen</a>
       <a href="wörterbuch.html" class="hover:bg-indigo-700 px-3 py-2 rounded-md">Wörterbuch</a>
@@ -35,24 +35,12 @@ const mobileNavHTML = `
 document.body.insertAdjacentHTML('afterbegin', mobileNavHTML); //fügt die Navbar in den Body teil ein
 
 const path = decodeURIComponent(window.location.pathname.split('/').pop()); // decodeURIComponent damit wird "ü" problem behoben :)
-const hrefs = [];
-/*let lee = "apple";
-const fruits = ['apple', 'banana', 'orange'];
-for (const fruit of fruits) {
-  if (lee == fruit) {
-    console.log("ja")
-  } else {
-    console.log("ne")
-  }
-} */
 console.log(path)
-const navLinks = document.querySelectorAll('#bottemNav a') // sucht nur nach der Bottem navBar
-console.log(navLinks.length);
-console.log(hrefs)
+const navLinksBottem = document.querySelectorAll('a'); // sucht nur nach der Bottem navBar
 
-for (const link of navLinks) {
+for (const link of navLinksBottem) {
   if (path === link.getAttribute('href')) {
     link.classList.remove('text-white', 'hover:bg-indigo-700');
-    link.classList.add('bg-indigo-700', 'text-amber-400');
+    link.classList.add('bg-indigo-700', 'text-amber-400', 'font-semibold');
   }
 }
