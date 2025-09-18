c6fb1c837cbbe347e17c158afac00290c2ec8872
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-09-18 21:21:48 +0200

    Workbox hizugefügt

---
+++ workbox-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "globDirectory": "./",
+    "globPatterns": [
+        "**/*.{html,css,js,png,json}"
+    ],
+    "swDest": "./service-worker.js",
+    "modifyURLPrefix": {
+        "": "/Vokabel-Trainer/"
+    }
+};
