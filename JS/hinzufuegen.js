20013fe97657d9a91825e935dd3f65f28cb21093
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-10-04 19:06:39 +0200

    hinzufügen html verändert das zuerst kategorie auswahl ist und dann zu den jeweiligen wörtern

--- JS/hinzufuegen.js
+++ JS/hinzufuegen.js
@@ -3,40 +3,40 @@ import { erstelleDropDown } from './test/kategorieauswahl.js';
 import { getVocabList, ladeVokList, speichereVokabelListe } from './test/vokabelabfrage-Data.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    erstelleDropDown();
     ladeVokList();
+    erstelleDropDown();
+
+})
+
+const addVocabForm = document.getElementById('addVocabForm');
+addVocabForm.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    const deutschesWort = document.getElementById('deutschesWort').value.trim();
+    const fremdSpracheWort = document.getElementById('englischesWort').value.trim();
+    const kategorie = document.getElementById('kategorie').value.trim();
+
+    // Neues Vokabelobjekt erstellen
+    const neueVokabel = {
+        wort: deutschesWort.trim(),
+        fremdsprache: fremdSpracheWort.trim()
+    };
+
+    const vocabList = getVocabList();
+    if (!vocabList[kategorie]) {
+        // Falls nicht, erstellen  ein neues leeres Array für diese Kategorie.
+        vocabList[kategorie] = [];
+    }
+    // Objekt wird in das zugehörige Katgegorie objekt gepushed
+    vocabList[kategorie].push(neueVokabel);
+    console.log(`Vokabel '${deutschesWort}' zu Kategorie '${kategorie}' hinzugefügt.`);
 
 
-    const addVocabForm = document.getElementById('addVocabForm');
-    addVocabForm.addEventListener('submit', function (event) {
+    speichereVokabelListe();
 
-        event.preventDefault();
-        const deutschesWort = document.getElementById('deutschesWort').value.trim();
-        const fremdSpracheWort = document.getElementById('englischesWort').value.trim();
-        const kategorie = document.getElementById('kategorie').value.trim();
-
-        // Neues Vokabelobjekt erstellen
-        const neueVokabel = {
-            wort: deutschesWort.trim(),
-            fremdsprache: fremdSpracheWort.trim()
-        };
-
-        const vocabList = getVocabList();
-        if (!vocabList[kategorie]) {
-            // Falls nicht, erstellen  ein neues leeres Array für diese Kategorie.
-            vocabList[kategorie] = [];
-        }
-        // Objekt wird in das zugehörige Katgegorie objekt gepushed
-        vocabList[kategorie].push(neueVokabel);
-        console.log(`Vokabel '${deutschesWort}' zu Kategorie '${kategorie}' hinzugefügt.`);
+    zeigeFeedback();
+    addVocabForm.reset();
 
 
-        speichereVokabelListe();
-
-        zeigeFeedback();
-        addVocabForm.reset();
-
-
-        console.log("Aktualisierte Vokabelliste:", vocabList);
-    });
+    console.log("Aktualisierte Vokabelliste:", vocabList);
 });
