0ed87a382e153641d3bd60e674a81516930938a7
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-10-07 16:38:47 +0200

    wenn Vokabelnkategorie ausgewählt wurde kommt man abjetztzt zur addVokabform

--- JS/hinzufuegen.js
+++ JS/hinzufuegen.js
@@ -1,19 +1,31 @@
 import { zeigeFeedback } from './hinzufügenUI.js'
 import { erstelleDropDown } from './test/kategorieauswahl.js';
-import { getVocabList, ladeVokList, speichereVokabelListe } from './test/vokabelabfrage-Data.js';
-
+import { getVocabList, kategorienAbrufen, ladeVokList, speichereVokabelListe } from './test/vokabelabfrage-Data.js';
+console.log("Bitte beide Felder ausfüllen!");
 document.addEventListener('DOMContentLoaded', () => {
     ladeVokList();
     erstelleDropDown();
+    const auswahl = document.getElementById('kategorieAuswahl');
+    const kategorieselect = document.getElementById('testSelection');
+    const addVocabForm = document.getElementById('addVocabForm')
+    auswahl.addEventListener('submit', function (event) {
 
-})
+        event.preventDefault();
+        const alleKategorien = kategorienAbrufen();
+        const kategorie = alleKategorien[kategorieselect.selectedIndex];
+        console.log(kategorie);
+        auswahl.classList.add('hidden');
+        addVocabForm.classList.remove('hidden')
 
+    })
+});
 const addVocabForm = document.getElementById('addVocabForm');
 addVocabForm.addEventListener('submit', function (event) {
 
     event.preventDefault();
     const alleKategorien = kategorienAbrufen();
     const kategorie = alleKategorien[dom.kategorieselect.selectedIndex];
+    console.log(kategorie);
     const deutschesWort = document.getElementById('deutschesWort').value.trim();
     const fremdSpracheWort = document.getElementById('englischesWort').value.trim();
 
